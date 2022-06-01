31339c2ee08f95a5aaa116f266d0f9a4a9941367
Petros <petros.chantz@gmail.com>
2022-06-01 22:08:27 +0200

    feat: adds About page

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import About from "./about/About";
 
 import Home from "./home/Home";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />}></Route>
-      </Routes>
+      </Routes> */}
+      <Home></Home>
+      <About></About>
     </>
   );
 }
