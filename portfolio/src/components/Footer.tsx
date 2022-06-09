5b8928aceb27d61b67d969fc05890d0488bd8f37
Petros <petros.chantz@gmail.com>
2022-06-09 12:32:14 +0200

    feat: adds footer

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <div className="flex flex-row p-8 bg-black md:px-16 lg:px-22">
+      <h5 className="self-center w-3/4 text-white footer">
+        petros chantzopoulos
+      </h5>
+      <div className="flex flex-col justify-end w-1/4 gap-1">
+        <p className="self-end text-white">v.2.3.1</p>
+        <p className="self-end text-white">Rotterdam - 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
