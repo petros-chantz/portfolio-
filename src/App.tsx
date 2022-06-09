8c2bb9dce5363d92e73ef46efe94667533e276ff
Petros <petros.chantz@gmail.com>
2022-06-09 14:53:06 +0200

    chore: restructures folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import About from "./pages/about/About";
 
 import Home from "./pages/home/Home";
+import Vision from "./pages/vision/Vision";
+import Work from "./pages/work/Work";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       </Routes> */}
       <Home></Home>
       <About></About>
+      <Work></Work>
+      <Vision></Vision>
+      <Footer></Footer>
     </>
   );
 }
