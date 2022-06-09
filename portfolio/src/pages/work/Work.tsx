b7265bcbfb69707f318d2d7d22904b602eb80b93
Petros <petros.chantz@gmail.com>
2022-06-09 12:33:55 +0200

    feat: changes work

--- portfolio/src/pages/work/Work.tsx
+++ portfolio/src/pages/work/Work.tsx
@@ -2,12 +2,12 @@ import HeaderPage from "../../components/HeaderPage";
 
 function Work() {
   return (
-    <div className="flex flex-col h-screen p-8 pt-16 bg-white lg:py-20 md:px-16 lg:px-22">
-      <HeaderPage pageName="work"></HeaderPage>
+    <div className="flex flex-col p-8 pt-16 bg-white h-96 md:h-screen lg:py-20 md:px-16 lg:px-22">
+      <h3 className="self-end">work</h3>
       <div className="flex flex-col justify-center h-full gap-10">
         <h2 className="self-center">Great things take time...</h2>
         <h2 className="self-center">
-          ...so, you'll have to wait a little bit longer ;)
+          so, you'll have to wait a little bit longer ;)
         </h2>
       </div>
     </div>
